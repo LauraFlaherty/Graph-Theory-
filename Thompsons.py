2604a5f6fcdec4953604a748b6654231a2865280
LauraFlaherty <g00348586@gmit.ie>
2019-03-31 00:29:04 +0000

    Combined Thompson's algorithm and the Shunting Yard Algorithm + added a few tests

--- Thompsons.py
+++ Thompsons.py
@@ -1,3 +1,54 @@
+#============================
+#Graph theory Project 
+#Regular expression Matching
+#Laura Flaherty
+
+#=============================
+# Shunting Yard Algorithm
+# Laura Flaherty
+# http://www.oxfordmathcenter.com/drupal7/node/628
+
+
+def shunt(infix):
+ """The shunting yard algorithm for converting infix regular expressions to postfix"""
+
+    # numbers reflect the order of precedence
+    # dictionaries are similar to arrays except they are indexed by strings rather than numbers
+    specials = {'*': 50, '.': 40, '|': 30, '+': 50, '?': 50}
+
+    pofix = ""
+    # put various operators that are not wanted in the pofix
+    stack = ""
+
+    #Loop through the string a character at a time
+    for c in infix:
+        if c == '(':
+            # if an open bracket, push to the stack
+            stack = stack + c
+            # if a closing bracket, pop from stack, push to output until open bracket
+        elif c == ')':
+            while stack[-1] != '(': 
+                pofix,stack = pofix + stack[-1],stack[:-1]
+            stack = stack[:-1]
+            #if its an operator, push to stack after popping lower or equal precedence
+            # operators from the top of the stack into ouput
+         
+        elif c in specials: # is the character c  in the dictionary
+            while stack and specials.get(c,0) <= specials.get(stack[-1],0):
+                pofix, stack = pofix + stack[-1], stack[:-1]
+             #push this special operator onto the stack           
+            stack = stack + c
+        else:
+        # append the regular characters from the infix to the postfix
+            pofix = pofix + c
+
+    while stack:
+        pofix, stack = pofix + stack[-1],stack[:-1]
+
+    return pofix
+#print(shunt("(a.b)|(c*.d)"))
+
+
 # Thompson's construction
 # Laura Flaherty
 
@@ -80,5 +131,9 @@ def compile(pofix):
     # nfastack should only have a single nfa on it at this point
     return nfastack.pop()
 
-print(compile("ab.cd.|"))
+#print(compile("ab.cd.|"))
 #print(compile("aa.*"))
+
+#A few tests.
+infixes = ["a.b.c*","a.(b|d).c*","(a.(b|d))*","a.(b.b)*.c"]
+strings = ["", "abc", "abbc", "abcc", "abad", "abbc"]
